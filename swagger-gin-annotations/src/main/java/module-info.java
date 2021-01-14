ad4452dcb78c5a4a9c1b1fd21bb7198641560fe4
wyd <wyudi@qq.com>
2021-01-14 16:39:57 +0800

    rename

--- swagger-gin-annotations/src/main/java/module-info.java
+++ swagger-gin-annotations/src/main/java/module-info.java
@@ -2,6 +2,7 @@
  * Created by admin on 2021/1/13 19:39:51.
  */
 module swagger.gin.annotations {
+    
     exports org.rainday.swagger.annotations;
     exports org.rainday.swagger.methods;
 }
--- swagger-models/src/main/java/module-info.java
+++ swagger-models/src/main/java/module-info.java
@@ -3,16 +3,17 @@
  */
 module swagger.models {
     exports io.swagger.v3.oas.models;
-    exports io.swagger.v3.oas.models.tags;
-    exports io.swagger.v3.oas.models.responses;
-    exports io.swagger.v3.oas.models.info;
+    exports io.swagger.v3.oas.models.callbacks;
     exports io.swagger.v3.oas.models.examples;
     exports io.swagger.v3.oas.models.headers;
+    exports io.swagger.v3.oas.models.info;
     exports io.swagger.v3.oas.models.links;
-    exports io.swagger.v3.oas.models.security;
-    exports io.swagger.v3.oas.models.parameters;
     exports io.swagger.v3.oas.models.media;
-    exports io.swagger.v3.oas.models.callbacks;
+    exports io.swagger.v3.oas.models.parameters;
+    exports io.swagger.v3.oas.models.responses;
+    exports io.swagger.v3.oas.models.security;
     exports io.swagger.v3.oas.models.servers;
+    exports io.swagger.v3.oas.models.tags;
+    
     requires com.fasterxml.jackson.annotation;
 }
